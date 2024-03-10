82be6976ec90b0b5af3827fd632348b80f2f6600
Hannah Grace Pugal <162258179+anyahpr@users.noreply.github.com>
2024-03-10 22:26:12 +0800

    Add files via upload

---
+++ MainMotorPH.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author anyah
+ */
+public class MainMotorPH { 
+        public void displayInfo(){
+        System.out.println("Welcome to Motor PH");
+    }
+}
+
